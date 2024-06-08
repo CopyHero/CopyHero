093425b0b322f151fc5e44cfcbb837548f14bcd4
jim <tang9527@gmail.com>
2024-06-08 13:41:22 +0100

    add: app info

--- app/api.py
+++ app/api.py
@@ -1,7 +1,22 @@
 from .api_coordinates import *
 from .app import app
+from mongoengine import get_connection
+from importlib.metadata import version
+from .settings import settings
 
 
 @app.get("/")
 def hello():
     return "hello world"
+
+
+@app.get("/info")
+def info():
+    connection = get_connection()
+    db_info = connection.server_info()
+    paddleocr_version = version("paddleocr")
+    return {
+        "mongodb_version": db_info["version"],
+        "paddleocr_version": paddleocr_version,
+        "copy_hero_version": settings.copy_hero_version,
+    }
